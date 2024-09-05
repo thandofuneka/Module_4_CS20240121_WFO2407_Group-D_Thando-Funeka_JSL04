4c640936b7c563ab2de95c34a30a92ca2907b90a
Thando <thandofuneka1@.com>
2024-09-05 17:12:27 +0200

    Ternary Operator

--- index.js
+++ index.js
@@ -5,14 +5,7 @@
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-
-if(userPreference == 'Sugar-free') {
-    console.log("Sugar-Free Monster Energy Drink!");
-} else if(userPreference == 'Regular') {
-    console.log("Regular Monster Energy Drink!");
-} else {
-    console.log('Undefined!');
-}
+console.log(userPreference === 'Sugar-free' ? 'Sugar-free' : 'Regular');
 
 
 // #### Challenge 2: Stock Check
@@ -20,11 +13,7 @@ if(userPreference == 'Sugar-free') {
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
 let cansLeft = 3; // Any number of cans
-if(cansLeft < 5) {
-    console.log("Time to restock!");
-} else {
-    console.log("We're stocked!");
-}
+console.log(cansLeft < 5 ? 'Time to restock!' : 'We\'re stocked!');
 
 
 // #### Challenge 3: Workout Intensity
@@ -32,11 +21,7 @@ if(cansLeft < 5) {
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
 
 let heartRate = 95; // Current heart rate in bpm
-if(heartRate < 100) {
-    console.log("Boost needed!");
-} else {
-    console.log("Energy Levels are high!");
-}
+console.log(heartRate < 100 ? 'Boost needed!' : 'Energy levels are high!');
 
 
 // #### Challenge 4: Temperature Suitability
@@ -44,11 +29,7 @@ if(heartRate < 100) {
 // if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
 
 let currentTemp = 4; // Current temperature in °C
-if(currentTemp < 5) {
-    console.log("Chilled to perfection!")
-} else {
-    console.log("Needs a cooler!")
-}
+console.log(currentTemp <= 5 ? 'Chilled to perfection!' : 'Needs a cooler!');
 
 
 // #### Challenge 5: Late Night Coding Session
@@ -62,12 +43,7 @@ let currentHour = 22;
 // Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
 // If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
 // Otherwise, print "Better stick to water."
-if( 7 < currentHour < 24) {
-    console.log("Unleash the beast!");
-} else {
-    console.log("Better stick to water");
-}
-
+console.log(currentHour >= 7 && currentHour < 24 ? 'Unleash the beast!' : 'Better stick to water.');
 
 
 
